d916a80c4d2c886cc7156776b176c72867b8cb07
Mohamed amine Lachheb <aminelachheb456@gmail.com>
2021-10-13 18:25:27 +0100

    simple stack

--- tests/stack.cpp
+++ tests/stack.cpp
@@ -4,29 +4,24 @@
 
 int main()
 {
-    // empty
-    std::cout << "empty: " << std::endl << std::endl;
+    // empty & push & pop & top & size
+    std::cout << "empty & push & pop & top: " << std::endl << std::endl;
     {
         std::stack<int , std::vector<int> > stk;
-    }
-    // pop
-    std::cout << "pop: " << std::endl << std::endl;
-    {
-        std::stack<int , std::vector<int> > stk;
-    }
-    // push
-    std::cout << "push: " << std::endl << std::endl;
-    {
-        std::stack<int , std::vector<int> > stk;
-    }
-    // size
-    std::cout << "size: " << std::endl << std::endl;
-    {
-        std::stack<int , std::vector<int> > stk;
-    }
-    // top
-    std::cout << "top: " << std::endl << std::endl;
-    {
-        std::stack<int , std::vector<int> > stk;
+
+        int sum = 0;
+        for (int i = 0; i < 14; i++)
+            stk.push((i + 1) * 37);
+        std::stack<int, std::vector<int> >::size_type sz = stk.size();
+        std::cout << "size of the stack is: " << sz << std::endl;
+        while (!stk.empty())
+        {
+            sum += stk.top();
+            std::cout << stk.top() << " ";
+            stk.pop();
+        }
+        std::cout << std::endl;
+        std::cout << "stack is empty: " << (stk.empty() ? "Yes" : "No") << std::endl;
+        std::cout << "sum of the stack: " << sum << std::endl;
     }
 }
