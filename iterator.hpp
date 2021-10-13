8787dc08e825e7b1dd82c8d571828ac30a24d619
Mohamed amine Lachheb <aminelachheb456@gmail.com>
2021-10-13 11:38:09 +0100

    difference

--- iterator.hpp
+++ iterator.hpp
@@ -13,15 +13,15 @@ struct iterator_traits
 	typedef typename Iterator::iterator_category	iterator_category;
 };
 
-template <class T>
-struct iterator_traits<T*>
-{
-	typedef ptrdiff_t					difference_type;
-    typedef T							value_type;
-    typedef T*							pointer;
-	typedef T&							reference;
-	typedef random_access_iterator_tag	iterator_category;
-};
+// template <class T>
+// struct iterator_traits<T*>
+// {
+// 	typedef ptrdiff_t					difference_type;
+//     typedef T							value_type;
+//     typedef T*							pointer;
+// 	typedef T&							reference;
+// 	typedef random_access_iterator_tag	iterator_category;
+// };
 
 template < class Category, class T, class Distance = ptrdiff_t,
 	class Pointer = T*, class Reference = T& >
@@ -40,10 +40,78 @@ struct forward_iterator_tag : public input_iterator_tag {};
 struct bidirectional_iterator_tag : public forward_iterator_tag {};
 struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
+
+
+
+
+
+
+template < class InputIterator >
+void    _advance(InputIterator &inputIterator, typename iterator_traits<InputIterator>::difference_type n, input_iterator_tag)
+{
+    while (n > 0) {
+        n--;
+        inputIterator++;
+    }
+}
+
+template < class BidirectionalIterator >
+void    _advance(BidirectionalIterator &bidirectionalIterator, typename iterator_traits<BidirectionalIterator>::difference_type n, bidirectional_iterator_tag)
+{
+    while (n > 0) {
+        n--;
+        bidirectionalIterator++;
+    }
+    while (n < 0) {
+        n++;
+        bidirectionalIterator++;
+    }
+}
+
+template < class RandomAccessIterator >
+void    _advance(RandomAccessIterator &randomAccessIterator, typename iterator_traits<RandomAccessIterator>::difference_type n, random_access_iterator_tag)
+{
+    randomAccessIterator += n;
+}
+
 template < class InputIterator >
 void	advance(InputIterator &inputIterator, typename iterator_traits<InputIterator>::difference_type n)
 {
-	
+	_advance(inputIterator, n, iterator_traits<InputIterator>::iterator_category());
 }
+
+
+
+
+
+template < class InputIterator >
+typename iterator_traits<InputIterator>::difference_type	_distance(InputIterator first, InputIterator last, input_iterator_tag)
+{
+	typename iterator_traits<InputIterator>::difference_type result = 0;
+
+	while (first != last) {
+		first++;
+		result++;
+	}
+	return (result);
+}
+
+template < class RandomAccessIterator >
+typename iterator_traits<RandomAccessIterator>::difference_type	_distance(RandomAccessIterator first, RandomAccessIterator last, input_iterator_tag)
+{
+	return last - first;
+}
+
+template < class InputIterator >
+iterator_traits<InputIterator>::difference_type	disctance(InputIterator first, InputIterator last)
+{
+	_distance(first, last, iterator_traits<InputIterator>::iterator_category());
+}
+
+
+
+
+
+
 
 #endif
