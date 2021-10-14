c2faab06b99d606d0ee8b12de47ea71cabd6c5ed
Mohamed amine Lachheb <aminelachheb456@gmail.com>
2021-10-14 15:56:17 +0100

    pair make_pair

--- tests/pair.cpp
+++ tests/pair.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
+    std::pair<std::string, int>  pr("Hello", 7);
 
+    std::cout << "pair of: " << pr.first << " and " << pr.second << std::endl;
+
+    std::pair<std::string, std::string> spr;
+    spr = std::make_pair("Hello ", "World");
+    std::cout << "pair of: " << spr.first << spr.second << std::endl;
 }
