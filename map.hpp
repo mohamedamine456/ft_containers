1bf258928778f83d0dd5caaec7c491beb12b5198
Mohamed amine Lachheb <aminelachheb456@gmail.com>
2021-11-02 18:42:47 +0100

    add non relational operators to map

--- map.hpp
+++ map.hpp
@@ -119,4 +119,39 @@ class ft::map
         // should add relational operators & swap
 };
 
+template < class Key, class T, class Compare, class Alloc >
+bool    operator== (const ft::map<Key, T, Compare, Alloc> &lhs, const ft::map<Key, T, Compare, Alloc> &rhs) {
+
+}
+
+template < class Key, class T, class Compare, class Alloc > 
+bool    operator!= (const ft::map<Key, T, Compare, Alloc> &lhs, const ft::map<Key, T, Compare, Alloc> &rhs) {
+    
+}
+
+template < class Key, class T, class Compare, class Alloc > 
+bool    operator< (const ft::map<Key, T, Compare, Alloc> &lhs, const ft::map<Key, T, Compare, Alloc> &rhs) {
+    
+}
+
+template < class Key, class T, class Compare, class Alloc > 
+bool    operator<= (const ft::map<Key, T, Compare, Alloc> &lhs, const ft::map<Key, T, Compare, Alloc> &rhs) {
+    
+}
+
+template < class Key, class T, class Compare, class Alloc > 
+bool    operator> (const ft::map<Key, T, Compare, Alloc> &lhs, const ft::map<Key, T, Compare, Alloc> &rhs) {
+    
+}
+
+template < class Key, class T, class Compare, class Alloc > 
+bool    operator>= (const ft::map<Key, T, Compare, Alloc> &lhs, const ft::map<Key, T, Compare, Alloc> &rhs) {
+    
+}
+
+template < class Key, class T, class Compare, class Alloc >
+void swap(ft::map<Key, T, Compare, Alloc> &lhs, ft::map<Key, T, Compare, Alloc> &rhs) {
+    lhs.swap(rhs);
+}
+
 #endif
