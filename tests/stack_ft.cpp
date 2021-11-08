6e2032b16eed7e1993601843bf9f5d7de0bc5db4
Mohamed amine Lachheb <aminelachheb456@gmail.com>
2021-11-08 10:54:20 +0100

    stack tests

--- tests/stack_ft.cpp
+++ tests/stack_ft.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <stack>
+#include "../vector.hpp"
+#include "../stack.hpp"
 
 int main()
 {
     // empty & push & pop & top & size
     std::cout << "empty & push & pop & top: " << std::endl << std::endl;
     {
-        std::stack<int , std::vector<int> > stk;
+        ft::stack<int , ft::vector<int> > stk;
 
         int sum = 0;
         for (int i = 0; i < 14; i++)
             stk.push((i + 1) * 37);
-        std::stack<int, std::vector<int> >::size_type sz = stk.size();
+        ft::stack<int, ft::vector<int> >::size_type sz = stk.size();
         std::cout << "size of the stack is: " << sz << std::endl;
         while (!stk.empty())
         {
