caf034fb7a1074638ed6d2652a5f28fa7e401b36
Mohamed amine Lachheb <aminelachheb456@gmail.com>
2021-11-11 11:12:43 +0100

    RBT insertion to check

--- utils/redBlackTree.hpp
+++ utils/redBlackTree.hpp
@@ -62,7 +62,47 @@ class RedBlackTree {
 		}
 
         void    insertNode(Node<K, V> *node) {
-			
+			BSTInsertion(node);
+			Node<K, V>	*tmp = this->root;
+			Node<K, V>	*tmpU;
+
+			while (node->parent->red == true)
+			{
+				if (node->parent == node->parent->parent->rightChild)
+				{
+					tmpU = node->parent->parent->leftChild;
+					if (tmpU.red == true) {
+						tmpU.red = false;
+						node->parent->red = false;
+						node->parent->parent->red = true;
+						node = node->parent->parent;
+					}
+					else if (node == node->parent->leftChild) {
+						node = node->parent;
+						leftRotation(node);
+					}
+					node->parent.red = false;
+					node->parent->parent->red = true;
+					rightRotation(node->parent->parent);
+				}
+				else {
+					tmpU = node->parent->parent->rightChild;
+					if (tmpU.red == true) {
+						tmpU.red = false;
+						node->parent->red = false;
+						node->parent->parent->red = true;
+						node = node->parent->parent;
+					}
+					else if (node == node->parent->rightChild) {
+						node = node->parent;
+						leftRotation(node);
+					}
+					node->parent.red = false;
+					node->parent->parent->red = true;
+					rightRotation(node->parent->parent);
+				}
+			}
+			this->root->red = false;
         }
 
 		void	BSTDelete(Node<K,V> *node, K key) {
@@ -101,17 +141,15 @@ class RedBlackTree {
 					tmp->data = tmp1->data;
 					tmp->rightChild = BSTDelete(tmp->rightChild, tmp1->data.key);
 				}
-			}
-
-				
+			}	
 		}
 
         void    deleteNode(Node<K, V> *node) {
-
+			BSTDelete(this->root, node->data.first);
         }
 
         void    deleteNode(K key) {
-
+			BSTDelete(this->root, key);
         }
 
 		void	rightRotation() {
