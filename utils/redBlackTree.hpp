cf54e33fe60d2c53539984582cde67a5a629396b
Mohamed amine Lachheb <aminelachheb456@gmail.com>
2021-11-11 10:17:30 +0100

    BST insertion deletion

--- utils/redBlackTree.hpp
+++ utils/redBlackTree.hpp
@@ -13,9 +13,10 @@ struct Node
 	Node<K, V>		*parent;
 };
 
-template < class K, class V>
+template < class K, class V, class Allocator = std::allocator<Node<K, V> > >
 class RedBlackTree {
     private:
+		Allocator	__alloc;
 		Node<K, V> *root;
 
     public:
@@ -38,9 +39,72 @@ class RedBlackTree {
             return *this;
         }
 
+		void	BSTInsertion(Node<K, V> *node) {
+			Node<K, V>	*tmp;
+			Node<K, V>	*tmp2;
+
+			tmp2 = nullptr;
+			tmp = this->root;
+			while (tmp != nullptr) {
+				tmp2 = tmp;
+				if (node->data.first < tmp->data.first)
+					tmp = tmp->leftChild;
+				else
+					tmp = tmp->rightChild;
+			}
+			node->parent = tmp2;
+			if (tmp2 == nullptr)
+				this->root = node;
+			else if (node->data.first < tmp2->data.first)
+				tmp2->leftChild = node;
+			else
+				tmp2->rightChild = node;
+		}
+
         void    insertNode(Node<K, V> *node) {
 			
         }
+
+		void	BSTDelete(Node<K,V> *node, K key) {
+			Node<K, V>	*tmp = node;
+			Node<K, V>	*tmp1;
+
+			if (tmp == nullptr)
+				return ;
+			else if (key < tmp->data.first)
+				BSTDelete(tmp->leftChild, key);
+			else if (key > tmp->data.first)
+				BSTDelete(tmp->rightChild, key);
+			else
+			{
+				if (tmp->leftChild == nullptr && tmp->rightChild == nullptr) {
+					__alloc.destroy(tmp);
+					__alloc.deallocate(tmp);
+					tmp = nullptr;
+				}
+				else if (tmp->leftChild == nullptr) {
+					tmp1 = tmp;
+					tmp = tmp->leftChild;
+					__alloc.destroy(tmp1);
+					__alloc.deallocate(tmp1);
+					tmp1 = nullptr
+				}
+				else if (tmp->rightChild == nullptr) {
+					tmp1 = tmp;
+					tmp = tmp->rightChild;
+					__alloc.destroy(tmp1);
+					__alloc.deallocate(tmp1);
+					tmp1 = nullptr
+				}
+				else {
+					tmp1 = minimum(tmp->rightChild);
+					tmp->data = tmp1->data;
+					tmp->rightChild = BSTDelete(tmp->rightChild, tmp1->data.key);
+				}
+			}
+
+				
+		}
 
         void    deleteNode(Node<K, V> *node) {
 
